8109721ab40b026e4455ca452c3a3438cb8f21a0
Vladimir077 <93789096+Vladimir077@users.noreply.github.com>
2022-10-26 18:08:24 +0300

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+
+function slidesPlugin(activesSlide = 0) {
+  const slides = document.querySelectorAll('.slide');
+
+slides[activesSlide].classList.add('active');
+
+for (const slide of slides) {
+  slide.addEventListener('click', () => {
+    clearActiveClasses();
+
+    slide.classList.add('active');
+  });
+}
+
+function clearActiveClasses() {
+   slides.forEach((slide) => {
+      slide.classList.remove('active');
+   });
+}
+}
+
+slidesPlugin(0);
